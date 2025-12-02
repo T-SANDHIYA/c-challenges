67e60fa21f6be66c44b5207d8c35c91700572999
T-SANDHIYA <sandhiyathirumal121@gmail.com>
2025-12-02 19:44:04 +0530

    Time: 3 ms (8.64%) | Memory: 43.4 MB (5.89%) - LeetSync

---
+++ 812-rotate-string/rotate-string.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        
+        if(s.equals(goal)){
+            return true;
+          
+        }
+        else{
+            for(int i=0;i<s.length();i++){
+                s = s.substring(1)+s.charAt(0);
+                if(s.equals(goal)){
+                    return true;
+                }
+
+            }
+        }
+        return false;
+        
+    }
+}
