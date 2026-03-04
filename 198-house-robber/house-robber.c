6a6dbafd3b56d6defe7097d3844b67b36704f315
T-SANDHIYA <sandhiyathirumal121@gmail.com>
2026-03-04 19:30:40 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (5.15%) - LeetSync

---
+++ 198-house-robber/house-robber.c
@@ -0,0 +1,18 @@
+int rob(int* nums, int numsSize) {
+    if (numsSize == 0) return 0;
+    if (numsSize == 1) return nums[0];
+
+    int prev2 = nums[0];
+    int prev1 = nums[0] > nums[1] ? nums[0] : nums[1];
+
+    for (int i = 2; i < numsSize; i++) {
+        int current = prev1 > (nums[i] + prev2) 
+                      ? prev1 
+                      : (nums[i] + prev2);
+
+        prev2 = prev1;
+        prev1 = current;
+    }
+
+    return prev1;
+}
